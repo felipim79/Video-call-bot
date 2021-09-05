a75c097a459b8fc1b25ca76c06d55198d46aaa22
rasindugimhan <83225696+rasindugimhan@users.noreply.github.com>
2021-09-05 08:25:16 +0530

    Update player.py

--- bot/video/player.py
+++ bot/video/player.py
@@ -160,12 +160,12 @@ allcmd = ["start", "help", f"start@{USERNAME}", f"help@{USERNAME}"] + admincmds
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/sl_bot_zone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/slbotzone"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/CozmoSupport_Official"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/CozmoUpdates"),
             ],
             [
-                InlineKeyboardButton("Source Code", url="https://github.com/youtubeslgeekshow/Video-call-bot"),
+                InlineKeyboardButton("༒ADD TO GROUP༒", url="t.me/TheCozmoBot?startgroup=true"),
             ]
-         ]
-    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/youtubeslgeekshow/Video-call-bot) 😉!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+          ]
+    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can see full help here😉!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
